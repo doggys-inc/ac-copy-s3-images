4921bbc6ad0cae0de4bdda03fbbb4c8ba218e57f
Sho Kohara <shokohara@users.noreply.github.com>
2024-08-03 19:50:53 +0900

    second commit

--- src/test/scala/MainSuite.scala
+++ src/test/scala/MainSuite.scala
@@ -1,18 +1,43 @@
-import cats.data.NonEmptyList
+import cats.data.EitherT
 import munit.FunSuite
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
 class MainSuite extends FunSuite:
-  test("progress"):
-    assertEquals(progress(1, NonEmptyList.fromListUnsafe(Array.fill(1000)(0).toList)), "0001/1000 (0%)")
-  test("progress and percentage"):
-    assertEquals(progress(21, NonEmptyList.fromListUnsafe(Array.fill(214)(0).toList)), "021/214 (9%)")
-  test("progress and percentage"):
-    assertEquals(progress(1, NonEmptyList.fromListUnsafe(Array.fill(1)(0).toList)), "1/1 (100%)")
-  test("progress and percentage"):
-    assertEquals(progress(1, NonEmptyList.fromListUnsafe(Array.fill(2)(0).toList)), "1/2 (50%)")
-  test(""):
-    assertEquals("123/124.jpg".takeWhile(_ != '/'), "123")
-  test(""):
-    assertEquals(List.empty.contains(true), false)
-  test(""):
-    assertEquals(None.contains(true), false)
+  test("Future.failedで例外を生成する場合"):
+    import cats.implicits.*
+    import cats.instances.future.*
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val myFuture: Future[Nothing] = Future.failed(new Exception())
+    val f: EitherT[Future, Throwable, String] = myFuture.attemptT
+    assert(Await.result(f.isLeft, Duration.Inf))
+  test("Futureの中でthrowする場合"):
+    import cats.implicits.*
+    import cats.instances.future.*
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val myFuture: Future[Nothing] = Future(throw new Exception())
+    val f: EitherT[Future, Throwable, String] = myFuture.attemptT
+    assert(Await.result(f.isLeft, Duration.Inf))
+  test("Exceptionを生成してtoEitherTする場合"):
+    import cats.implicits.*
+    import cats.instances.future.*
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val f: EitherT[Future, Throwable, String] = new Exception().asLeft.toEitherT[Future]
+    assert(Await.result(f.isLeft, Duration.Inf))
+  test("new Exception().raiseError.attemptT"):
+    import cats.implicits.*
+    import cats.instances.future.*
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val future: Future[String] = new Exception().raiseError[Future, String]
+    val f: EitherT[Future, Throwable, String] = future.attemptT
+    assert(Await.result(f.isLeft, Duration.Inf))
+
+  import java.time.LocalDateTime
+  import java.time.format.DateTimeFormatter.*
+  val dts = "2018-12-13T19:19"
+  LocalDateTime.parse(dts, ISO_DATE_TIME)
